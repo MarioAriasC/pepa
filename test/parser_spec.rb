e206b18ffdcd4e6eb3c6fee77263bc63e8b52355
Mario Arias <mario@marioarias.org>
2022-04-13 14:27:21 +0800

    let statements

---
+++ test/parser_spec.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+
+describe 'Parser' do
+  before do
+    # Do nothing
+  end
+
+  after do
+    # Do nothing
+  end
+
+  context 'when condition' do
+    it 'succeeds' do
+      skip 'Not implemented'
+    end
+  end
+end
